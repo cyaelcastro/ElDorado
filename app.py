6ae9012408f00439e09083a954065645a01470ad
Yael Castro <cyaelcc94@gmail.com>
2022-07-03 23:14:03 -0500

    Set up HTTP methods and create /print endpoint

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def hello():
     return "Hello World from Docker!"
+
+
+@app.route('/print', methods=["POST"])
+def print_content():
+    data = request.args.get("content")
+    return f"Received content: {data}"
